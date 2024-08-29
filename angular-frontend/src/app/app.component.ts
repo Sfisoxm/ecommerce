1f28b49ddd4d17456ef55a899b0d084ff322a0c8
cphyso <sifisoxmhlongo@gmail.com>
2024-08-29 12:41:24 +0200

    initial commit backend

--- angular-frontend/src/app/app.component.ts
+++ angular-frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CoolbitsComponent } from "./coolbits/coolbits.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CoolbitsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
